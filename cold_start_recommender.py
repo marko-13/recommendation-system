5c066fb0034a5b64ddcb666d6b34330fbab5b082
Marko Jovanovic <marko07031997@gmail.com>
2020-02-26 11:14:49 +0100

    recommender

--- cold_start_recommender.py
+++ cold_start_recommender.py
@@ -68,3 +68,78 @@ class Recommender_bot():
         conf = row[5][location_counter]
 
         return flag_ok, conf
+
+
+# ---------------------------------------------------------------FUNS---------------------------------------------------
+
+
+# Finds all forms based on rules from selected algorithm
+def find_all_forms(df, algo, columns, first_form_index):
+    # columns contains names of all columns whose values are True(1)
+    all_forms_cols = df.columns[first_form_index:]
+
+    # dict where key is form name and value is summed up confidence from all occurrences in rules
+    confidence_dict = {}
+    for col in all_forms_cols:
+        flag, confidence_sum = checks_given_form(col, algo, columns)
+        if flag:
+            confidence_dict[col] = confidence_sum
+
+    confidence_dict = {k: v for k, v in sorted(confidence_dict.items(), key=lambda item: item[1], reverse=True)}
+    return confidence_dict
+
+
+# checks if given form is suitable
+def checks_given_form(form_name, algo, columns):
+    # columns contains names of all columns whose values are True(1)
+
+    if algo == 'apriori':
+        df = pd.read_csv(f"models/apriori/{form_name}/relevant.csv", delimiter=',')
+    elif algo == 'fpg':
+        df = pd.read_csv(f"models/fp_growth/{form_name}/relevant.csv", delimiter=',')
+    else:
+        print("INVALID ALGORITHM")
+        exit(1)
+
+    confidence_sum = 0
+
+    flag = False
+    for ind, row in df.iterrows():
+
+        if form_name in row[2]:
+            flag, confidence = check_if_form_is_suitable(row, form_name, columns)
+            if flag:
+                confidence_sum += confidence
+
+    print("CONFIDENCE SUM: " + str(confidence_sum) + "\n\n")
+    if confidence_sum != 0:
+        flag = True
+    return flag, confidence_sum
+
+
+def check_if_form_is_suitable(row, form_name, columns):
+    # print(row)
+
+    flag_ok = True
+    for wanted_column in columns:
+        if wanted_column not in row[1]:
+            flag_ok = False
+            return flag_ok, 0
+
+    conf = 0
+    # ovde se nalaze confidence
+    row[5]
+
+    location_counter = 0
+    for el in row[1]:
+        if el == form_name:
+            break
+        location_counter += location_counter
+
+    conf = row[8]
+    # print(row[1])
+    # print(row[2])
+    # print("CONF: " + str(conf))
+    # print("\n\n")
+
+    return flag_ok, conf
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import insurance_recommendation as ir
 import rule_miner as rm
 import form_nn
 import datetime
+import cold_start_recommender as csr
 
 from insurance_recommendation import *
 
@@ -22,12 +23,20 @@ if __name__ == "__main__":
     # form_nn.run_training_for_all_forms(df, first_form_index)
     # print("\n\n\nGOTOV ANN: " + str(datetime.datetime.now()))
 
-    print("\n\n\nPOCEO FPG: " + str(datetime.datetime.now()))
-    # Run FP Growth
-    rm.find_all_rules(df, first_form_index, algo='fpg')
-    print("\n\n\nGOTOV FPG: " + str(datetime.datetime.now()))
+    # print("\n\n\nPOCEO FPG: " + str(datetime.datetime.now()))
+    # # Run FP Growth
+    # rm.find_all_rules(df, first_form_index, algo='fpg')
+    # print("\n\n\nGOTOV FPG: " + str(datetime.datetime.now()))
+    #
+    # print("\n\n\nPOCEO APRIORI: " + str(datetime.datetime.now()))
+    # # Run apriori
+    # rm.find_all_rules(df, first_form_index, algo='apriori')
+    # print("\n\n\nGOTOV APRIORI: " + str(datetime.datetime.now()))
 
-    print("\n\n\nPOCEO APRIORI: " + str(datetime.datetime.now()))
-    # Run apriori
-    rm.find_all_rules(df, first_form_index, algo='apriori')
-    print("\n\n\nGOTOV APRIORI: " + str(datetime.datetime.now()))
+
+    print("\n\n\nPOCEO RECOMMENDATION: " + str(datetime.datetime.now()))
+    # Run cold start recommender
+    rec_forms = csr.find_all_forms(df, 'fpg', ['General Aggregate Coverage Limits',
+                                                               'Occurrence: General Liability'], first_form_index)
+    print("\n\n\nGOTOV RECOMMENDATION: " + str(datetime.datetime.now()))
+    print(rec_forms)
