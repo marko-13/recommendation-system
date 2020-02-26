1adf03154fb52e21c1fa6532613e60cdc5195248
Marko Jovanovic <marko07031997@gmail.com>
2020-02-26 09:58:29 +0100

    creating bot

--- rule_miner.py
+++ rule_miner.py
@@ -7,41 +7,6 @@ from mlxtend.frequent_patterns import fpgrowth
 from mlxtend.frequent_patterns import apriori
 import mlxtend
 
-# def get_rules(df, first_form_ind, target_form_ind):
-
-#     all_recs = df.values.tolist()
-#     print(df)
-#     print("\n\n\n\n")
-
-#     # extract column names where ones are located in df for each row
-#     mother_list = []
-#     for index, row in df.iterrows():
-#         child_list = []
-#         col_ind = 0
-#         for el in row:
-#             if col_ind == first_form_ind:
-#                 break
-#             if el == 1:
-#                 child_list.append(df.columns[col_ind])
-
-#             col_ind += 1
-#         print(df.iloc[index])
-#         if df.iloc[index, :][df.columns[target_form_ind]] == 1:
-#             child_list.append(df.columns[target_form_ind])
-
-#         mother_list.append(child_list)
-
-#     association_rules = apriori(mother_list, min_support=0.02, min_confidence=0.8, max_length=None, min_lift=0)
-#     association_results = list(association_rules)
-
-#     with open('log/rules_f1.pickle', 'wb') as f:
-#         pickle.dump(association_results, f)
-
-#     # print(all_recs)
-#     print(len(association_results))
-#     for i in range (5):
-#         print(association_results[i])
-
 
 def find_all_rules(df, first_form_index, algo):
     all_forms_cols = df.columns[first_form_index:]
@@ -155,65 +120,3 @@ def _save_rules(df, form_name, file_name, algo):
     else:
         print("Could not save rules")
         exit(1)
-
-
-# Finds all forms based on rules from selected algorithm
-def find_all_forms(df, algo, columns, first_form_index):
-    # columns contains names of all columns whose values are True(1)
-    all_forms_cols = df.columns[first_form_index:]
-
-    # dict where key is form name and value is summed up confidence from all occurrences in rules
-    confidence_dict = {}
-    for col in all_forms_cols:
-        flag, confidence_sum = checks_given_form(col, algo, columns)
-        if flag:
-            confidence_dict[col] = confidence_sum
-
-    return confidence_dict
-
-
-# checks if given form is suitable
-def checks_given_form(form_name, algo, columns):
-    # columns contains names of all columns whose values are True(1)
-
-    if algo == 'apriori':
-        df = pd.read_csv(f"models/apriori/{form_name}/relevant.csv", delimiter=',')
-    elif algo == 'fpg':
-        df = pd.read_csv(f"models/fp_growth/{form_name}/relevant.csv", delimiter=',')
-    else:
-        print("INVALID ALGORITHM")
-        exit(1)
-
-    confidence_sum = 0
-    for ind, row in df.iterrows():
-        if form_name in row[1]:
-            # DO CHECKING
-            flag, confidence = check_if_form_is_suitable(row, form_name)
-            if flag:
-                confidence_sum += confidence
-
-    return flag, confidence_sum
-
-
-def check_if_form_is_suitable(row, form_name, columns):
-    print(row)
-
-    flag_ok = True
-    for wanted_column in columns:
-        if wanted_column not in row[1]:
-            flag_ok = False
-            return flag_ok, 0
-
-    conf = 0
-    # ovde se nalaze confidence
-    row[5]
-
-    location_counter = 0
-    for el in row[1]:
-        if el == form_name:
-            break
-        location_counter += location_counter
-        
-    conf = row[5][location_counter]
-
-    return flag_ok, conf
